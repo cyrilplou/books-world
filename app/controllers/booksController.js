5da3967a50bd640a1e665d503bcf2c3b19051c33
Cyril <cyril.plou@gmail.com>
2024-11-27 18:25:10 +0100

    Setting up the first endpoint for the API

---
+++ app/controllers/booksController.js
@@ -0,0 +1,49 @@
+import axios from "axios";
+
+export const booksController = {
+
+    async findBook(req,res){
+// L'API Google Books gère pas bien les champs vides c'est pourquoi je définis une string dans le cas ou le champs est vide qui n'aura aucun effet dans la requête. Autrement, l'ensemble de la query et de son paramètre son passé dans la requête.
+
+        let bookTitle = '';
+        let bookauthor='';
+        // Si ma requête est vide j'insère tout de même une string pour éviter que l'API ne plante.
+        if(req.query.title.length<=0){
+            bookTitle = '';
+        }else{
+            bookTitle= `"${req.query.title}"`
+        }
+        // Idem avec l'auteur.
+
+        if(req.query.author.length<=0){
+            bookauthor = '';
+        }else{
+            bookauthor= `+inauthor:${req.query.author}`
+        }
+     
+        // Je définis ma requête.
+        const apiUrl = `https://www.googleapis.com/books/v1/volumes?q=${bookTitle}${bookauthor}&orderBy=newest&langRestrict=fr`;
+
+        // Je lance ma requête vers Google Books API
+        const response = await axios.get(apiUrl);
+    
+        // Récupération des données.
+        const books = response.data.items;
+        // Les informations intéressantes se trouvent dans un objet volumeInfo du tableau Books. Mes informations utiles pour la suite après .volumeInfo    :
+        // ID google du livre (c'est ce qui sera stocker dans la BDD lorsqu'un utilisateur ajoute un livre à sa librairie).
+        // -> Titre : .title STRING
+        // -> Auteur : .author STRING
+        // -> date de publication : .publishedDate STRING 
+        // -> ISBN : industryIdentifieres[0].identifier STRING
+        // -> genre : .categories Array
+        // -> image de couv : .imageLinks.thumbnail STRING
+
+
+        // J'envoie ma page ainsi que le résultat de ma requête.
+        res.render('search', {books})
+        console.log(books)
+
+    },
+
+
+}
--- app/router.js
+++ app/router.js
@@ -1,5 +1,11 @@
 import { Router } from 'express';
 
+import { booksController } from './controllers/booksController.js';
 export const router = Router();
 
 
+router.get('/', (req,res)=>{
+    res.render('home')
+})
+
+router.get('/search', booksController.findBook)
